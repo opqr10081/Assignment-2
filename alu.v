c208c27ce591089c1aebcdac7e62e3de2d7631c2
opqr10081 <opqr10081@hanyang.ac.kr>
2026-04-13 13:48:03 +0900

    Create alu.v

---
+++ alu.v
@@ -0,0 +1,17 @@
+module alu(
+    input [31:0] a, b,
+    input [2:0] alu_ctrl,
+    output reg [31:0] result
+);
+
+always @(*) begin
+    case(alu_ctrl)
+        3'b000: result = a & b;
+        3'b001: result = a | b; 
+        3'b010: result = a + b;
+        3'b110: result = a - b;
+        default: result = 0;
+    endcase
+end
+
+endmodule
